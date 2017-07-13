9e9ac811708ea0349b7c2685d34e168657863517
mnikn <329121948@qq.com>
2017-07-13 22:16:45 +0800

    update preview ui

--- src/note/preview/note-preview.module.ts
+++ src/note/preview/note-preview.module.ts
@@ -1,9 +1,13 @@
-import {NgModule} from "@angular/core";
-import {NotePreviewComponent} from "./note-preview.component";
+import {NgModule} from '@angular/core';
+import {NotePreviewComponent} from './note-preview.component';
+import {CommonModule} from '@angular/common';
 
 @NgModule({
   declarations: [
     NotePreviewComponent
+  ],
+  imports: [
+    CommonModule
   ],
   exports: [
     NotePreviewComponent
