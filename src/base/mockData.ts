a4275db48bea8a8d1fedee9fa686e9da948e2201
mnikn <329121948@qq.com>
2017-07-13 19:32:44 +0800

    make preview support html

--- src/base/mockData.ts
+++ src/base/mockData.ts
@@ -3,11 +3,14 @@ import {Tag} from './models/tag';
 import {Content} from './models/content';
 
 export const TAGS: Tag[] = [{name: 'Feeling'},{name: 'Life'}];
-export const CONTENT: Content = {mdContent: '# Good', htmlContent: '<h1>Good</h1>'};
+export const CONTENT1: Content = {mdContent: '# Good', htmlContent: '<h1>Good</h1>'};
+export const CONTENT2: Content = {mdContent: '# Fine', htmlContent: '<h1>Fine</h1>'};
+export const CONTENT3: Content = {mdContent: '# Well', htmlContent: '<h1>Well</h1>'};
+export const CONTENT4: Content = {mdContent: '# Fuck', htmlContent: '<h1>Fuck</h1>'};
 
 export const ARTICLES: Article[] = [
-  {title: 'good', content: CONTENT, tags: TAGS, insertDate: new Date(2016, 5, 4)},
-  {title: 'nice', content: CONTENT, tags: TAGS, insertDate: new Date(2016, 3, 4)},
-  {title: 'well', content: CONTENT, tags: TAGS, insertDate: new Date(2017, 4, 4)},
-  {title: 'fuck', content: CONTENT, tags: TAGS, insertDate: new Date(2017, 2, 4)}
+  {title: 'good', content: CONTENT1, tags: TAGS, insertDate: new Date(2016, 5, 4)},
+  {title: 'nice', content: CONTENT2, tags: TAGS, insertDate: new Date(2016, 3, 4)},
+  {title: 'well', content: CONTENT3, tags: TAGS, insertDate: new Date(2017, 4, 4)},
+  {title: 'fuck', content: CONTENT4, tags: TAGS, insertDate: new Date(2017, 2, 4)}
 ];
