dbdd55baca6669203eed36075e764b8c0380b23a
mnikn <329121948@qq.com>
2017-07-16 12:54:53 +0800

    fix name convention

--- src/common/app-template.module.ts
+++ src/common/app-template.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { SuiSidebarModule } from 'ng2-semantic-ui';
-import { InfoHeaderComponent } from './infoHeader/infoHeader.component';
+import { InfoHeaderComponent } from './info-header/info-header.component';
 import { AppTemplateComponent } from './app-template.component';
 import { AppRoutingModule } from '../app/app-routing.moudle';
 
--- src/common/info-header/info-header.component.ts
+++ src/common/info-header/info-header.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'infoHeader',
-  templateUrl: './infoHeader.component.html'
+  templateUrl: './info-header.component.html'
 })
 
 export class InfoHeaderComponent {
