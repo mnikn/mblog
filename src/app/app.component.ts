e868ebf44b590f9b19a26591c543cd021566a8c9
mnikn <329121948@qq.com>
2017-07-22 12:16:37 +0800

    add ipc render service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { IpcRenderService } from '../core/services/ipcRender/ipc-render.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  providers: [IpcRenderService]
 })
 export class AppComponent {
 }
