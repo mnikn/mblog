ea915c32422c818b2bead472625278d11860850b
mnikn <329121948@qq.com>
2017-07-23 11:34:32 +0800

    rename

---
+++ src/app/shared/additionBar/addition-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'addition-bar',
+  templateUrl: './addition-bar.component.html'
+})
+
+export class AdditionBarComponent {
+
+}
--- src/app/shared/app-template.module.ts
+++ src/app/shared/app-template.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { SuiSidebarModule } from 'ng2-semantic-ui';
-import { InfoHeaderComponent } from './infoHeader/info-header.component';
+import { AdditionBarComponent } from './additionBar/addition-bar.component';
 import { AppTemplateComponent } from './app-template.component';
 import { AppRoutingModule } from '../app-routing.moudle';
 
 @NgModule({
   declarations: [
     SidebarComponent,
-    InfoHeaderComponent,
+    AdditionBarComponent,
     AppTemplateComponent
   ],
   imports: [
@@ -17,7 +17,7 @@ import { AppRoutingModule } from '../app-routing.moudle';
   ],
   exports: [
     SidebarComponent,
-    InfoHeaderComponent,
+    AdditionBarComponent,
     AppTemplateComponent
   ]
 })
