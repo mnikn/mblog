6acfb2857c85b20bd1c046887946a4c61fe3a1a3
mnikn <329121948@qq.com>
2017-07-23 11:39:49 +0800

    move note preview

--- src/app/note/note-info.module.ts
+++ src/app/note/note-info.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { NoteListModule } from './list/note-list.module';
 import { NoteListComponent } from './list/note-list.component';
-import { NotePreviewModule } from './preview/note-preview.module';
-import { NotePreviewComponent } from './preview/note-preview.component';
+import { NotePreviewModule } from '../shared/notePreview/note-preview.module';
+import { NotePreviewComponent } from '../shared/notePreview/note-preview.component';
 import { NoteInfoComponent } from './note-info.component';
 import { CommonModule } from '@angular/common';
 import { AppTemplateModule } from '../shared/app-template.module';
