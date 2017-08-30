cdaba9886885dfc7943a0424474f246ba273eb9d
mnikn <329121948@qq.com>
2017-08-30 22:29:44 +0800

    remove useless code

--- src/app/main/shared/toolbar/toolbar.component.ts
+++ src/app/main/shared/toolbar/toolbar.component.ts
@@ -19,7 +19,6 @@ export class ToolbarComponent implements OnInit{
   private selectStatus: ARTICLE_STATUS;
 
   constructor(public dataService: ArticleDataService,
-              public modalService: SuiModalService,
               private router: Router,
               private route: ActivatedRoute) {
   }
@@ -29,49 +28,6 @@ export class ToolbarComponent implements OnInit{
       this.selectStatus = Number(params.get('status'));
     });
   }
-
-  // public onAdd() {
-  //   let article = new Article();
-  //   article.status = this.selectStatus;
-  //   this.dataService.createItem(article);
-  // }
-  //
-  // public onDelete(popup: IPopup) {
-  //   if (!this.dataService.getSelected()) {
-  //     popup.open();
-  //     setTimeout(() => {
-  //       popup.close();
-  //     }, 1000);
-  //     return;
-  //   }
-  //
-  //   this.modalService
-  //     .open(new ConfirmModal('确定要删除笔记吗？'))
-  //     .onApprove(() => {
-  //       this.dataService.deleteItem(this.dataService.getSelected());
-  //       this.dataService.setSelected(null);
-  //     });
-  // }
-  //
-  // public onRefresh() {
-  //   this.dataService.refresh();
-  // }
-
-  // public onDeploy() {
-  //   this.isDeploying = true;
-  //   let dir = Context.config.blogRoot.replace(' ', '\\ ');
-  //   let command = 'cd ' + dir + ' && hexo g && hexo d';
-  //   electron.remote.require('child_process').exec(command, (error, stdout, stderr) => {
-  //     console.log(error);
-  //     console.log(stdout);
-  //     console.log(stderr);
-  //   });
-  //
-  //   // deleteItem later
-  //   setTimeout(() => {
-  //     this.isDeploying = false;
-  //   }, 10000);
-  // }
 
   public onEdit() {
     this.router.navigate(['/edit', this.dataService.getSelected().id]);
